50c8b39c29748300a12cdfbfb1b2307071d9bf9e
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2023-07-21 20:47:46 +0200

    Make better binary

--- 05-lwt-cli-app/bin/main.ml
+++ 05-lwt-cli-app/bin/main.ml
@@ -1,18 +1,50 @@
 (*
   Run with:
 
-    PGHOST=localhost PGDATABASE=caqti_study PGPORT=5433 dune exec ./bin/main.exe
+    make -C .. db-reset && PGHOST=localhost PGDATABASE=caqti_study PGPORT=5433 dune exec ./bin/main.exe
 *)
 
-(* FIXME: broken at runtime *)
-let will_add a b : (int, 'err) result Lwt.t =
-  let ( let* ) = Lwt_result.bind in
-  let* conn = Repo.Init.connect () in
-  Repo.Exec.add conn a b
+module Init = Repo.Init
+module Bibliography = Repo.Bibliography
 
+(*
+ * UTILS
+ *)
+let info_log fmt = Printf.printf ("[INFO] " ^^ fmt ^^ "\n%!")
+let err_log fmt = Printf.printf ("[ERROR] " ^^ fmt ^^ "\n%!")
+
+let longest_book_name bibliography =
+  List.fold_left
+    (fun longest
+         ( _id,
+           (book_name, (_author_fname, (_author_middle_name, _author_lname))) ) ->
+      max longest (String.length book_name))
+    0 bibliography
+
+let print_item padding
+    (_id, (book_name, (author_fname, (_author_middle_name, author_lname)))) =
+  Printf.printf "%-*s: %s %s\n" padding book_name author_fname author_lname
+
+(*
+ * BOOTSTRAP
+ *)
 let () =
-  match Lwt_main.run (will_add 1 2) with
-  | Error err ->
-      print_endline "Oops, we encountered an error!";
-      print_endline (Caqti_error.show err)
-  | Ok n -> Printf.printf "DB says 1+2=%d\n" n
+  let work : ('bibliography, 'error) result Lwt.t =
+    let open Lwt_result.Syntax in
+    let* conn = Init.connect () in
+    let* () = Init.create_tables conn in
+    let* () = Init.seed conn in
+    Bibliography.ls conn ()
+  in
+
+  match Lwt_main.run work with
+  | Error e -> err_log "%s" (Caqti_error.show e)
+  | Ok bibliography ->
+      let longest = longest_book_name bibliography in
+      ();
+      info_log "Setup OK!";
+      print_newline ();
+      print_endline "Bibliography";
+      print_endline "============";
+      print_newline ();
+      List.iter (print_item (longest + 1)) bibliography
