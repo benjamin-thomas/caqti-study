c217db67991ef45510511984c24b20ef79141a1a
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2023-07-22 15:41:22 +0200

    Cleanup: move REPLs into their own modules

--- 05-lwt-cli-app/bin/cli_app.ml
+++ 05-lwt-cli-app/bin/cli_app.ml
@@ -42,62 +42,68 @@ let print_header str =
   print_string "> ";
   flush Out_channel.stdout
 
-let get_args () =
-  print_header "author";
-  match get_line () with None -> [] | Some str -> String.split_on_char ' ' str
-
 (** It is assumed these command won't fail at runtime, given the fact they've been tested.
     Regardless, we probably don't want to print a specific error message in this context *)
-let run promise f =
-  match Lwt_main.run promise with
+let run_promise p f =
+  match Lwt_main.run p with
   | Error _ -> print_endline "Sorry, something went wrong!"
   | Ok x -> f x
 
-let print_author (id, first_name) = printf "%d) %s\n%!" id first_name
+module Author_REPL = struct
+  let print_author (id, first_name) = printf "%d) %s\n%!" id first_name
 
-let print_author_opt = function
-  | None -> print_endline "Author not found!"
-  | Some x -> print_author x
+  let print_author_opt = function
+    | None -> print_endline "Author not found!"
+    | Some x -> print_author x
 
-let rec author_repl conn args =
-  let again = author_repl conn in
-  match args with
-  | [] | "main" :: [] ->
-      (* Ctrl+D or typed "main" *)
-      print_newline ()
-  | "ls" :: [] ->
-      run (Author.ls' conn) @@ List.iter print_author;
-      again (get_args ())
-  | "insert" :: [] ->
-      print_endline "will start author insert mode";
-      again (get_args ())
-  | [ "find"; id ] ->
-      run (Author.find_by_id conn id) print_author_opt;
-      again (get_args ())
-  | _ ->
-      print_endline "Unknown author command or sub-command!";
-      again (get_args ())
+  let get_args () =
+    print_header "author";
+    match get_line () with
+    | None -> []
+    | Some str -> String.split_on_char ' ' str
 
-let rec main_repl conn =
-  print_header "main";
-  match get_line () with
-  | Some line -> (
-      match line |> String.split_on_char ' ' with
-      | "quit" :: [] -> print_endline "Bye bye!"
-      | "help" :: [] ->
-          print_endline help;
-          main_repl conn
-      | "author" :: args ->
-          author_repl conn args;
-          main_repl conn
-      | _ ->
-          print_endline "Unknown command!";
-          main_repl conn)
-  | None ->
-      (* Ctrl+D *)
-      print_newline ();
-      print_endline "See you next time!";
-      ()
+  let rec run conn args =
+    let again = run conn in
+    match args with
+    | [] | "main" :: [] ->
+        (* Ctrl+D or typed "main" *)
+        print_newline ()
+    | "ls" :: [] ->
+        run_promise (Author.ls' conn) @@ List.iter print_author;
+        again (get_args ())
+    | "insert" :: [] ->
+        print_endline "will start author insert mode";
+        again (get_args ())
+    | [ "find"; id ] ->
+        run_promise (Author.find_by_id conn id) print_author_opt;
+        again (get_args ())
+    | _ ->
+        print_endline "Unknown author command or sub-command!";
+        again (get_args ())
+end
+
+module Main_REPL = struct
+  let rec run conn =
+    print_header "main";
+    match get_line () with
+    | Some line -> (
+        match line |> String.split_on_char ' ' with
+        | "quit" :: [] -> print_endline "Bye bye!"
+        | "help" :: [] ->
+            print_endline help;
+            run conn
+        | "author" :: args ->
+            Author_REPL.run conn args;
+            run conn
+        | _ ->
+            print_endline "Unknown command!";
+            run conn)
+    | None ->
+        (* Ctrl+D *)
+        print_newline ();
+        print_endline "See you next time!";
+        ()
+end
 
 let () =
   match Lwt_main.run (Init.connect ()) with
@@ -106,4 +112,4 @@ let () =
       prerr_endline (Caqti_error.show err)
   | Ok conn ->
       print_endline {|Welcome to our CLI app! (type "help" to learn more)|};
-      main_repl conn
+      Main_REPL.run conn
