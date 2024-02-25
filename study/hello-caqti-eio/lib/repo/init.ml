af3ee511c34e9e3734fe8536005e935fed8ecc5c
Benjamin <benjamin.guy.thomas@gmail.com>
2024-02-25 17:34:35 +0100

    Simplify: don't suggest 2 different connection styles

--- study/hello-caqti-eio/lib/repo/init.ml
+++ study/hello-caqti-eio/lib/repo/init.ml
@@ -15,37 +15,20 @@ let get_uri () =
       Printf.sprintf "postgresql://%s:%s/%s" pg_host pg_port pg_database
   | None -> "postgresql://"
 
-let new_stdenv env : Caqti_eio.stdenv =
-  object
-    method clock = Eio.Stdenv.clock env
-    method mono_clock = Eio.Stdenv.mono_clock env
-    method net = Eio.Stdenv.net env
-  end
+(**
 
-(*
-  In eio, we need to send a "switch" to the callee, for it to do among other
-  things resources cleanup.
+  In eio, we need to send a "switch" to the callee. This can enable it to do,
+  among other things, resources cleanup, message cancellation, etc.
 
   Thusly, we can't hold on to a connection as we did in the previous projects.
   For when the switch goes out of scope, the connection becomes stale.
-  So we will instead pass-in the function [f] to do work, callback-style.
+  So we will instead work with a callback.
+
+  We also use the [:>] operator to force a type coersion between the more
+  general [Eio_unix.Stdenv.base] type, to the more specific (but still
+  compatible) [Caqti_eio.stdenv] type.
 *)
 let with_conn f =
-  let ( let* ) = Result.bind in
-  let connect =
-    let uri = get_uri () in
-    Caqti_eio_unix.connect (Uri.of_string uri)
-  in
+  let uri = Uri.of_string @@ get_uri () in
   Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun sw ->
-  let* conn = connect ~sw ~stdenv:(new_stdenv env) in
-  f conn
-
-(*
-  It turns out that this callback style is already generally supported by Caqti's API.
-  We could apply this "with_connection" pattern to any connection ([lwt], [async], etc.)
-*)
-let with_conn' f =
-  let uri = get_uri () in
-  Eio_main.run @@ fun env ->
-  Caqti_eio_unix.with_connection (Uri.of_string uri) ~stdenv:(new_stdenv env) f
+  Caqti_eio_unix.with_connection uri ~stdenv:(env :> Caqti_eio.stdenv) f
